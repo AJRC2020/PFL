6fb769cc539a30b8cc01993c35051a95c0e470a5
frenato00 <up201908044@up.pt>
2021-11-27 23:29:21 +0000

    code commented except mulAdd & mulLista

--- BigNumber.hs
+++ BigNumber.hs
@@ -4,27 +4,31 @@ import Data.Char (digitToInt, intToDigit)
 
 type BigNumber = (Char , [Int])
 
+--turns a number in a String to BigNumber
 scanner :: String -> BigNumber
 scanner a | head a == '-' = ('-', map digitToInt (drop 1 a))
           | otherwise = ('+', map digitToInt a)
 
+--turns a BigNumber to a string
 output :: BigNumber -> String
 output a | fst a == '-' = '-' : map intToDigit (takeZeros(snd a))
          | otherwise = map intToDigit (takeZeros(snd a))
 
-
+-- ensures every element in the list is only one digit long, if not, it separates them
 somaCheck :: [Int] -> [Int]
 somaCheck [x] | x < 10 = [x]
               | otherwise = [x `mod` 10, x `div` 10]
 somaCheck (x:xs) | x < 10 = x : somaCheck xs
                  | otherwise = x `mod` 10 : somaCheck (head xs + x `div` 10 : drop 1 xs)
 
+-- returns a list with the sum of both argument lists elements
 somaLista :: [Int] -> [Int] -> [Int]
 somaLista [] [] = []
 somaLista (a:as) [] = a : somaLista as []
 somaLista [] (b:bs) = b : somaLista [] bs
 somaLista (a:as) (b:bs) = a + b : somaLista as bs
 
+-- this auxiliar function ensures that after a subtraction no element of the list is negative
 subCheck :: [Int] -> [Int]
 subCheck [x] | x == 0 = []
              | x < 0 = [x + 10]
@@ -32,22 +36,25 @@ subCheck [x] | x == 0 = []
 subCheck (x:xs) | x < 0 = x + 10 : subCheck (head xs - 1 : drop 1 xs)
                 | otherwise  = x : subCheck xs
 
+
+-- returns a list with the result of the subtraction of the elements of the first list by the elements of the second
 subLista :: [Int] -> [Int] -> [Int]
 subLista [] [] = []
 subLista (a:as) [] = a : subLista as []
 subLista [] (b:bs) = b : subLista [] bs
 subLista (a:as) (b:bs) = a - b : subLista as bs
 
+-- appends a specified number n of elements of value 0 to the a list
 addZeros :: [Int] -> Int -> [Int]
 addZeros a 0 = a
 addZeros a n = 0 : addZeros a (n-1)
 
+-- removes all redundant zeros to the left
 takeZeros :: [Int] -> [Int]
 takeZeros [] = [0]
--- takeZeros [a]       | a == 0  = []
---                     | otherwise = [a]
 takeZeros (a:as)    | a == 0  = takeZeros as
                     | otherwise = a:as
+
 
 mulAdd :: [[Int]] -> [Int]
 mulAdd [] = [0]
@@ -56,49 +63,41 @@ mulAdd (x:xs) = somaCheck (somaLista (addZeros x (length xs)) (mulAdd xs))
 mulLista :: [Int] -> [Int] -> [[Int]]
 mulLista a = map (\b -> somaCheck (map (*b) a))
 
+-- compares the values of the two lists with the same lenght.
+-- returns 0 if the lists have the same value, 1 if the first list has a greater value and 2 if it is the second one
 maiorsame :: BigNumber -> BigNumber -> Int
 maiorsame (_, []) (_, []) = 0
 maiorsame (a, b:bs) (c, d:ds) | b > d = 1
                               | b < d = 2
                               | otherwise = maiorsame (a, bs) (c, ds)
 
+-- compares the values of the two lists with arbitrary lenghts.
+--returns the list with greater value on it, if they are equal returns the first
 maior :: BigNumber -> BigNumber -> BigNumber
 maior a b | length (snd a) > length (snd b) = a
           | length (snd a) < length (snd b) = b
           | otherwise = if maiorsame a b == 1 || maiorsame a b == 0 then a else b
 
+-- compares the values of the two lists with arbitrary lenghts.
+-- returns 0 if the lists have the same value, 1 if the first list has a greater value and 2 if it is the second one
 menor :: BigNumber -> BigNumber -> Int
 menor a b | length (snd a) > length (snd b) = 1
           | length (snd a) < length (snd b) = 2
           | otherwise = maiorsame a b
 
+-- takes an Int and separates all of its digits into a list
 listSpliter :: Int -> [Int]
 listSpliter 0 = []
 listSpliter a = listSpliter (div a 10) ++ [mod a 10]
 
-divLista :: [Int] -> [Int] -> [Int]
-divLista a [] = []
-divLista [] b = []
-divLista [a] [b] = [div a b]
-divLista (a:as) [b] | a>b = listSpliter(div a b) ++ divLista (a`mod`b:as) [b]
-                    | otherwise = divLista ((a*10+head as):tail as) [b]
-
-divListaMany :: [Int] -> [Int] -> [Int]
-divListaMany a [] = []
-divListaMany a [b] = divLista a [b]
-divListaMany a (b:bs) = divListaMany a ((b*10+head bs) : tail bs)
-
-divSub :: BigNumber -> BigNumber -> BigNumber
-divSub a b  | menor a b == 2 = ('+',[0])
-            | otherwise = somaBN ('+',[1]) (divSub (subBN a b) b)
-
+-- takes two BigNumbers and returns a list with the difference in value of the order of magnitude of the two
 lengthDif :: BigNumber -> BigNumber -> [Int]
 lengthDif a b   | menor a b == 2 =  [1]
                 |otherwise  = aux (length (snd a) - length (snd b)) where
                     aux 0 = [1]
                     aux n = 1 : replicate (n-1) 0
 
-
+-- takes two positive BigNumbers and calculates the quotient of the divison between them
 divSub2 :: BigNumber -> BigNumber -> BigNumber
 divSub2 a b = aux a b ('+',lengthDif a b) where
     aux a b ('+',[1])   | menor a b == 2 = ('+',[0])
@@ -106,18 +105,14 @@ divSub2 a b = aux a b ('+',lengthDif a b) where
     aux a b delta       | menor a (mulBN b delta) == 2 = aux a b ('+', init (snd delta))
                         | otherwise = somaBN delta (aux (subBN a (mulBN delta b)) b delta)
 
-divSubRest :: BigNumber -> BigNumber -> (BigNumber, BigNumber)
-divSubRest a b =(q, subBN a (mulBN q b)) where q = divSub a b
-
-factorial :: Int -> BigNumber
-factorial n = scanner (show (product [1..n]))
-
+-- calculates the sum of two BigNumbers
 somaBN :: BigNumber -> BigNumber -> BigNumber
 somaBN a b | fst a == '+' && fst b == '+' = ('+', reverse (somaCheck (somaLista (reverse (snd a)) (reverse (snd b)))))
            | fst a == '-' && fst b == '-' = ('-', reverse (somaCheck (somaLista (reverse (snd a)) (reverse (snd b)))))
            | maior a b == a =  (fst (maior a b), reverse (subCheck (subLista (reverse (snd a)) (reverse (snd b)))))
            | otherwise = (fst (maior a b), reverse (subCheck (subLista (reverse (snd b)) (reverse (snd a)))))
 
+-- calculates the difference of two BigNumbers
 subBN :: BigNumber -> BigNumber -> BigNumber
 subBN a b | fst a == '+' && fst b == '-' = ('+', reverse (somaCheck (somaLista (reverse (snd a)) (reverse (snd b)))))
           | fst a == '-' && fst b == '+' = ('-', reverse (somaCheck (somaLista (reverse (snd a)) (reverse (snd b)))))
@@ -126,13 +121,16 @@ subBN a b | fst a == '+' && fst b == '-' = ('+', reverse (somaCheck (somaLista (
           | fst a == '-' && fst b == '-' && maior a b == a = ('-', takeZeros (reverse (subCheck (subLista (reverse (snd a)) (reverse (snd b))))))
           | fst a == '-' && fst b == '-' && maior a b == b = ('+', takeZeros (reverse (subCheck (subLista (reverse (snd b)) (reverse (snd a))))))
 
+-- calculates the product of two BigNumbers
 mulBN :: BigNumber -> BigNumber -> BigNumber
 mulBN a b | fst a == fst b = ('+', reverse (mulAdd (reverse (mulLista (reverse (snd a)) (reverse (snd b))))))
           | otherwise = ('-', reverse (mulAdd (reverse (mulLista (reverse (snd a)) (reverse (snd b))))))
 
+-- calculates the quotient and rest of the divison between two BigNumbers using divSub2
 divBN :: BigNumber -> BigNumber -> (BigNumber, BigNumber)
 divBN a b = let res = divSub2 a b in (res, subBN a (mulBN res b))
 
+-- calculates the division between two BigNumbers and checks for cases of "divide by 0"
 safeDivBN :: BigNumber -> BigNumber -> Maybe (BigNumber, BigNumber)
 safeDivBN a b | output b == "0" = Nothing
               | otherwise = Just (divBN a b)
--- Fib.hs
+++ Fib.hs
@@ -2,37 +2,43 @@ import Prelude
 import Distribution.Compat.CharParsing (integral)
 import BigNumber
 
+
+-- recursive definition of the fibonacci sequence, starting from the base cases 0 and 1
 fibRec :: (Integral a) => a -> a
 fibRec 0 = 0
 fibRec 1 = 1
 fibRec n = fibRec (n-1) + fibRec(n-2)
 
+-- dynamic definition of the fibonacci sequence, making use of previous values in order to not repeat calculations
 fibLista :: Int -> Int
 fibLista n = go n (0, 1)
     where
         go n (a, b) | n == 0 = a
                     | otherwise = go (n-1) (b, a + b)
 
+-- definition of an infinit list containing the elements of the fibonacci sequence
 fibs :: [Integer]
 fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
 
+-- returns the element of index n from the fibonacci sequence list
 fibListaInfinita :: (Integral a) => a -> a
 fibListaInfinita n = fromIntegral  (fibs !! fromIntegral n)
 
+-- recursive definition of the fibonacci sequence in BigNumber
 fibRecBN :: Integral a => a -> BigNumber
--- fibRecBN :: Integer -> BigNumber
 fibRecBN 0 = ('+',[0])
 fibRecBN 1 = ('+',[1])
 fibRecBN n = somaBN a b where
     a = fibRecBN(n-1)
     b = fibRecBN(n-2)
 
+-- dynamic definition of the fibonacci sequence in BigNumber, making use of previous values in order to not repeat calculations
 fibListaBN :: Integral a => a -> BigNumber
 fibListaBN n = aux n (('+',[0]), ('+',[1])) where
     aux n (a, b)    | n == 0 = a
                     | otherwise = aux (n-1) (b, somaBN a b)
 
-
+-- returns the element of index n from the BigNumber fibonacci sequence list
 fibListaInfinitaBN :: Integral a => a -> BigNumber
 fibListaInfinitaBN n = aux !! fromIntegral n where
     aux = ('+',[0]) : ('+',[1]) : zipWith somaBN aux (tail aux)
