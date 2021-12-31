c69acea8a2ccd136e8cefdd80efad84ce8bb30f2
frenato00 <up201908044@up.pt>
2021-12-31 10:02:44 +0000

    start TP2

---
+++ TP2/README.md
@@ -0,0 +1,15 @@
+# Projeto PFL
+## Trabalho Prático 2:
+### "Jostle" by Mark Steere
+<br>
+
+### Grupo T2_Jostle_3: <br>
+Francisco Renato Barbosa Pires up201908044 <br>
+Alberto José Ribeiro da Cunha up201906325 
+
+<br>
+
+## Descrição do jogo
+
+...
+
---
+++ TP2/src/main.pl
@@ -0,0 +1,8 @@
+play
+display_game(+GameState)
+initial_state(+Size, -GameState)
+move(+GameState, +Move, -NewGameState)
+game_over(+GameState, -Winner)
+valid_moves(+GameState, -ListOfMoves)
+value(+GameState, +Player, -Value)
+choose_move(+GameState, +Level, -Move)
