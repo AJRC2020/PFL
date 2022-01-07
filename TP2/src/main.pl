f666830d311716426ab8521ca11cc7ecfc2a2dee
frenato00 <up201908044@up.pt>
2022-01-07 13:20:41 +0000

    0.1

--- TP2/src/main.pl
+++ TP2/src/main.pl
@@ -1,8 +1,23 @@
-play
-display_game(+GameState)
-initial_state(+Size, -GameState)
-move(+GameState, +Move, -NewGameState)
-game_over(+GameState, -Winner)
-valid_moves(+GameState, -ListOfMoves)
-value(+GameState, +Player, -Value)
-choose_move(+GameState, +Level, -Move)
+% play()
+play().
+
+% display_game(+GameState)
+display_game(GameState).
+
+% initial_state(+Size, -GameState)
+initial_state(Size, GameState).
+
+% move(+GameState, +Move, -NewGameState)
+move(+GameState, Move, NewGameState).
+
+% game_over(+GameState, -Winner)
+game_over(+GameState, Winner).
+
+% valid_moves(+GameState, -ListOfMoves)
+valid_moves(+GameState, ListOfMoves).
+
+% value(+GameState, +Player, -Value)
+value(+GameState, Player, Value).
+
+% choose_move(+GameState, +Level, -Move)
+choose_move(GameState, Level, Move).
