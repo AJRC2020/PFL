cb3b0bb8c72aa8c38c4f9d6635079b4d17da7c41
frenato00 <up201908044@up.pt>
2022-01-14 08:42:30 +0000

    0.3

--- TP2/src/display.pl
+++ TP2/src/display.pl
@@ -1,10 +1,12 @@
+
+
 % display_game(+GameState)
+% Recebendo o estado de jogo atual (que inclui o jogador que efetuará a próxima jogada) mostra o estado atual do tabuleiro
 display_game(GameState):-
-    display_start_screen,
-    display_start_menu(X),
-    display_clear,
-    write(X),nl,
-    write(GameState).
+    display_clear.
+    % construct board
+    
+    
 
 display_clear :- write('\33\[2J').
 
@@ -22,10 +24,11 @@ display_start_screen :-
     write('*                                                    *'), nl,
     write('******************************************************'), nl,
     nl,
-    write('Press any key to continue: '),
+    write('Press Enter to continue: '),
     skip_line.
 
 display_start_menu(X) :-
+    repeat,
     display_clear,
     write('******************************************************'), nl,
     write('*  1. Player vs Player                               *'), nl,
@@ -39,18 +42,17 @@ display_start_menu(X) :-
     write('*                                                    *'), nl,
     write('******************************************************'), nl,
     nl,
-    write('Choose an option (1-3): '),
-    get_code(Input),
+    read(Input),
     skip_line,
-    X is Input,
-    display_start_menu_error(X).
+    display_start_menu_error(Input),
+    X is Input.
 
-display_start_menu_error(1).
-display_start_menu_error(2).
-display_start_menu_error(3).
+display_start_menu_error(1) :- !.
+display_start_menu_error(2) :- !.
+display_start_menu_error(3) :- !.
 display_start_menu_error(X) :-
     display_clear,
     write('Invalid input, choose a valid option.'),nl,
-    write('Press any key to continue: '),
+    write('Press Enter to continue: '),
     skip_line,
-    display_start_menu(X).
+    fail.
--- TP2/src/main.pl
+++ TP2/src/main.pl
@@ -1,23 +1,49 @@
 :- ensure_loaded(display).
 % play()
 play :-
-    display_game(0).
+    display_start_screen,
+    % display_start_menu(X).
+    % initial_state(Size, GameState),
+    % display_game(GameState),
 
 
-% % initial_state(+Size, -GameState)
-% initial_state(Size, GameState).
 
-% % move(+GameState, +Move, -NewGameState)
-% move(+GameState, Move, NewGameState).
 
-% % game_over(+GameState, -Winner)
-% game_over(+GameState, Winner).
+% initial_state(+Size, -GameState)
+% Recebe o tamanho do tabuleiro como argumento e devolve o estado inicial do jogo
+initial_state(0, GameState):- !.
+initial_state(Size, GameState):-
+    GameState is [[]],
+    initial_state(Size,GameState, 0).
+initial_state(Size, GameState, Index) :-
+    Size =:= Index, !,
+    append(GameState,[],GameState),
+    Index is Index + 1,
+    initial_state(Size1, GameState, Index).
 
-% % valid_moves(+GameState, -ListOfMoves)
-% valid_moves(+GameState, ListOfMoves).
 
-% % value(+GameState, +Player, -Value)
-% value(+GameState, Player, Value).
+make_rows(0,Row).
+make_rows(Size,Row):-
+    make_rows()
+    append(Row,0,Row),
+    Size1 is Size-1,
+    make_rows(Size1,Row).
+% move(+GameState, +Move, -NewGameState)
+% Validação e execução de uma jogada, obtendo o novo estado do jogo
+% move(GameState, Move, NewGameState).
 
-% % choose_move(+GameState, +Level, -Move)
+% game_over(+GameState, -Winner)
+% Verificação da situação de fim do jogo, com identificação do vencedor
+% game_over(GameState, Winner).
+
+% valid_moves(+GameState, -ListOfMoves)
+% Obtenção de lista com jogadas possíveis
+% valid_moves(GameState, ListOfMoves).
+
+% value(+GameState, +Player, -Value)
+% Forma(s) de avaliação do estado do jogo do ponto de vista de um jogador
+% value(GameState, Player, Value).
+
+% choose_move(+GameState, +Level, -Move)
+% Escolha da jogada a efetuar pelo computador, dependendo do nível de dificuldade
 % choose_move(GameState, Level, Move).
